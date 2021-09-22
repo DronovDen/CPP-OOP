0ef72da957551f2d9baf50150c249aa6902d3bf4
DronovDen <d.dronov@g.nsu.ru>
2021-09-22 23:20:58 +0700

    added constructors, destructors, push_back, pop_back implementations

--- RNA/RNA.cpp
+++ RNA/RNA.cpp
@@ -1,54 +1,106 @@
 #include "RNA.hpp"
 
-  // CONSTRUCTORS
-  RNA::RNA(Nucl N, size_t const capacity)
+// CONSTRUCTORS
+RNA::RNA(Nucl N, size_t capacity)
+{
+  this->uint_len = (capacity / nucles_per_elem) + (capacity % nucles_per_elem != 0);
+  this->nucleref = new uint32_t[uint_len];
+  memset(nucleref, 0, uint_len);
+  this->n_number = capacity;
+  for (size_t i = 0; i < n_number; i++)
   {
-    n_number = capacity;
-    uint_len = capacity * 2 / sizeof(uint32_t) / 8 + 1;
-    if (capacity == 0)
-      nucleref = nullptr;
-    nucleref = new uint32_t[uint_len];
-    for(size_t i = 0; i < n_number; i++){
-      
-    }
+    nucleref[i / nucles_per_elem] |= (uint32_t)(N << (sizeof(uint32_t) * 8 - 2 - (i % uint_nucles) * 2));
   }
+}
 
-  RNA::RNA()
+RNA::RNA(size_t capacity)
+{
+  this->uint_len = (capacity / nucles_per_elem) + (capacity % nucles_per_elem != 0);
+  this->nucleref = new uint32_t[uint_len];
+  memset(nucleref, 0, uint_len);
+  this->n_number = capacity;
+}
+
+RNA::RNA()
+{
+  this->uint_len = 0;
+  this->n_number = 0;
+  this->nucleref = nullptr
+}
+
+//COPY CONSTRUCTOR
+RNA::RNA(const RNA &other)
+{
+  for (size_t i = 0; i < other.len; i++)
   {
-    this->uint_len = 0;
-    this->n_number = 0;
-    this->nucleref = nullptr
+    nucleref.push_back(other.nucleref.get(i));
   }
+  this->len = other.len;
+}
 
-  //COPY CONSTRUCTOR
-  RNA::RNA(RNA &other)
+// DESTRUCTOR
+~RNA()
+{
+  if (nucleref)
   {
-    for (size_t i = 0; i < other.len; i++)
-    {
-      nucleref.push_back(other.nucleref.get(i));
-    }
-    this->len = other.len;
+    delete[] nucleref;
+    nucleref = nullptr;
   }
+}
 
-  // DESTRUCTOR
-  ~RNA()
+size_t RNA::get_uints()
+{
+  return uint_len;
+}
+
+size_t RNA::get_number_of_nucles()
+{
+  return n_number;
+}
+
+void RNA::resize()
+{
+  uint32_t *array = new uint32_t[uint_len * 2];
+  memset(array, 0, uint_len * 2);
+  uint_len = uint_len * 2;
+  for (int i = 0; i < uints; i++)
   {
-    this->nucleref.~bit_vec();
-    std::cout << "RNA";
+    array[i] = nucleref[i];
   }
+  delete[] nucleref;
+  nucleref = array;
+}
 
-  void add();
+void RNA::push_back(Nucl N)
+{
+  new_n_num = n_number + 1;
+  if (new_n_num / nucs_per_elem + (new_n_num % nucs_per_elem >= 1) > uint_len)
+  {
+    resize();
+  }
+  n_number = new_n_num;
+  nucleref[n_number / nucles_per_elem] |=
+      (N << (2 * (nucles_per_elem - (n_number % nucles_per_elem)));
+}
 
-  /* operator== ();
+Nucl RNA::pop_back()
+{
+  n_number--;
+  return (Nucl)((data[len / nucles_per_elem] >> (nucles_per_elem - 1 - len % nucles_per_elem) * 2) &
+                BIT_MASK);
+}
+
+void add();
+
+/* operator== ();
   operator!= ();
-  operator ! ();
+  operator! ();
   operator[] ();
 
   isComplementary(RNK &);
   split(size_t index);
 
   DNK(RNK&, RNK&); */
-};
 
 int main()
 {
--- RNA/RNA.hpp
+++ RNA/RNA.hpp
@@ -6,25 +6,28 @@
 
 #define BIT_MASK 0x3
 
+const size_t nucles_per_elem = (sizeof(uint32_t) * 8 / 2);
+
 enum Nucl : uint32_t
 {
-    A = 0x0,
-    C = 0x1,
-    G = 0x2,
-    T = 0x3
+  A = 0x0,
+  C = 0x1,
+  G = 0x2,
+  T = 0x3
 };
 
 class RNA
 {
 private:
-  uint32_t* nucleref;
-  size_t uint_len;
+  uint32_t *nucleref; //reference of nucleotids
+  size_t uint_len;  //number of uints 
   //size_t capacity; //max number of nucleotids
   size_t n_number; //number of nucles in RNA reference
 
 public:
   // CONSTRUCTORS
-  RNA(Nucl N, size_t const capacity);
+  RNA(Nucl N, size_t capacity);
+  RNA(size_t capacity);
   RNA();
 
   //COPY CONSTRUCTOR
@@ -32,6 +35,14 @@ public:
 
   // DESTRUCTOR
   ~RNA();
+
+  size_t RNA::get_uints()
+
+  size_t RNA::get_number_of_nucles()
+
+  void resize();
+
+  void push_back(Nucl N);
 
   void add();
 
