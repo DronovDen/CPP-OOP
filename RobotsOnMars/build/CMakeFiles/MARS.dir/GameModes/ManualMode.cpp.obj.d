62af6283915ff87575c266a7c05f0e2a4e002dfd
DronovDen <90022919+DronovDen@users.noreply.github.com>
2021-12-24 01:40:28 +0700

    Delete RobotsOnMars/build directory

--- RobotsOnMars/build/CMakeFiles/MARS.dir/GameModes/ManualMode.cpp.obj.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/MARS.dir/GameModes/ManualMode.cpp.obj: \
- C:\My\ Documents\GitHub\OOP_3sem\RobotsOnMars\GameModes\ManualMode.cpp \
- C:\My\ Documents\GitHub\OOP_3sem\RobotsOnMars\GameModes\ManualMode.h \
- C:\My\ Documents\GitHub\OOP_3sem\RobotsOnMars\GameModes\ModeBasement.h \
- C:/My\ Documents/GitHub/OOP_3sem/RobotsOnMars/Commands/ManualCommands.h \
- C:/My\ Documents/GitHub/OOP_3sem/RobotsOnMars/Commands/ICommand.h
--- RobotsOnMars/build/CMakeFiles/MARS.dir/GameModes/ModeBasement.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MARS.dir/GameModes/ModeBasement.cpp.obj: \
- C:\My\ Documents\GitHub\OOP_3sem\RobotsOnMars\GameModes\ModeBasement.cpp \
- C:\My\ Documents\GitHub\OOP_3sem\RobotsOnMars\GameModes\ModeBasement.h
